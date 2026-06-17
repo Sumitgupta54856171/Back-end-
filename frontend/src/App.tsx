d5b35bb1ff4eb9452a8a3bfc049fa2664d76805d
Sumitgupta54856171 <sg54856171@gmail.com>
2026-06-17 16:28:02 +0000

    feat: implement role-based routing for user and host views

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 
-import './App.css'
 import Home from './Home'
 import Login from './Login';
 import Signup from './Signup';
@@ -11,23 +10,46 @@ import Sidebar from './ui/Navbar.tsx';
 import { Routes, Route } from 'react-router-dom';
 import HostLogin from './component/Host/HostLogin.tsx';
 import HostSigin from './component/Host/HostSigin.tsx';
-
+import Homepage from './Pages/Homepage.tsx';
+import PublicNavbar from './component/public/Navbar.tsx';
+import Wishlistspage from './Pages/Whislistspage.tsx';
+import Layout from './component/Host/Layout.tsx';
+import { useContext } from 'react';
+import { ConditionContext } from './context/context.tsx';
+import HostDashboard from './Pages/HostDashboard.tsx';
+import Dashboard from './component/Host/Dashboard.tsx';
+import Listings from './component/Host/Listings.tsx';
 function App() {
-  return (
-    <>
+  const checkrole = useContext(ConditionContext)
+
+  return checkrole === 'user' ? (
+    
+    <PublicNavbar>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/host/login" element={<HostLogin/>}></Route>
-        <Route path="/host/sigin" element={<HostSigin/>}></Route>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/host/login" element={<Home />}></Route>
+        <Route path="/wish" element={<Wishlistspage />}></Route>
+        
         <Route path="/login" element={<Login />}></Route>
         <Route path="/signup" element={<Signup />}></Route>
         <Route path="/bookingform" element={<Bookingform />}></Route>
         <Route path="/loginpage" element={<Loginpage />}></Route>
-        <Route path='/data' element={<DataTableDemo/>}></Route>
-        <Route path='/side' element={<Sidebar/>}></Route>
+        <Route path="/data" element={<DataTableDemo />}></Route>
+        <Route path="/side" element={<Sidebar />}></Route>
       </Routes>
-    </>
+    </PublicNavbar>
+  ) : (
+      <Layout>
+      <Routes>
+        <Route path="/host/dashboard" element={<HostDashboard />}></Route>
+        <Route path='/host/listings' element={<Listings/>}></Route>
+    
+    </Routes>
+    </Layout>
+    
+    
   )
+  
 }
 
 export default App
